e6779ffb69dfadce026b9016a4a757949ad918a5
OFFICE\d.uryupin <d.uryupin@stroytlm.ru>
2023-07-27 08:33:57 +0300

    update

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "math.h"
 
 /*
  *
@@ -7,7 +8,7 @@
  *
  */
 
-typedef void(* f)(float x);
+typedef float (* f)(float x);
 
 float f1(float x) {
     return 0.6f * x + 3;
@@ -22,12 +23,16 @@ float f3(float x) {
 }
 
 float find_root(float x1, float x2, float eps, f func) {
+    float delta_x = fabsf(func(x1) - func(x2));
+    float root = 0;
     for (float i = x1; i < x2; i += eps) {
 
     }
+    return root;
 }
 
 int main() {
-
+    float root = find_root(-5,5,0.0001,f1);
+    printf("root = %f", root);
     return 0;
 }
